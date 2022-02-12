fb4bff51ac5e7add9e2997be018790aa620a7c95
Andrei Androne <andrei.androne@outlook.com>
2022-02-12 20:17:03 +0200

    OOP_SumOfArrayRecursivity

---
+++ OOP_SumOfArrayRecursivity/OOP_SumOfArrayRecursivity/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OOP_SumOfArrayRecursivity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int sum = SumOfArrayHelper.SumOfArray(array, array.Length);
+            Console.WriteLine($"Sum of vector elements: {sum}");
+        }
+    }
+}
---
+++ OOP_SumOfArrayRecursivity/OOP_SumOfArrayRecursivity/SumOfArrayHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_SumOfArrayRecursivity
+{
+    public static class SumOfArrayHelper
+    {
+        public static int SumOfArray(int[] array, int size, int index = 0)
+        {
+            if (array is null || array.Length == 0)
+            {
+                return -1;
+            }
+
+            if (index == size - 1)
+            {
+                return array[index];
+            }
+
+            int sum = SumOfArray(array, size, index + 1);
+
+            sum += array[index];
+
+            return sum;
+        }
+    }
+}
